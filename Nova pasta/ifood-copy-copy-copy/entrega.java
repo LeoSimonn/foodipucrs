9d81c543695334bf6818e81cbf15e11d625056ee
murilaine32 <murilodf32@gmail.com>
2023-11-06 20:00:52 -0300

    subindo

---
+++ Nova pasta/ifood-copy-copy-copy/entrega.java
@@ -0,0 +1,35 @@
+
+
+public class entrega {
+    private double pedido;
+    private double entregador;
+    private String statusEntrega;
+    private String detalhesEntrega;
+
+    public entrega(double pedido, double entregador, String statusEntrega) {
+        this.pedido = pedido;
+        this.entregador = entregador;
+        this.statusEntrega = statusEntrega;
+        this.detalhesEntrega = "Pedido: " + pedido + ", Entregador: " + entregador + ", Status de Entrega: " + statusEntrega;
+    }
+
+    public double getPedido() {
+        return pedido;
+    }
+
+    public double getEntregador() {
+        return entregador;
+    }
+
+    public String getStatusEntrega() {
+        return statusEntrega;
+    }
+
+    public String getDetalhesEntrega() {
+        return detalhesEntrega;
+    }
+}
+
+
+
+
